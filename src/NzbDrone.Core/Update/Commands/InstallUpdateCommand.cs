7e54c819324ec7c0236563aded681b5063e3658b
Qstick <qstick@gmail.com>
2020-04-23 18:37:21 -0400

    Fixed: Don't lock command queue if updating is disabled

--- src/NzbDrone.Core/Update/Commands/InstallUpdateCommand.cs
+++ src/NzbDrone.Core/Update/Commands/InstallUpdateCommand.cs
@@ -2,10 +2,12 @@ using NzbDrone.Core.Messaging.Commands;
 
 namespace NzbDrone.Core.Update.Commands
 {
-    public class ApplicationUpdateCommand : Command
+    public class InstallUpdateCommand : Command
     {
         public override bool SendUpdatesToClient => true;
+        public override bool IsExclusive => true;
 
         public override string CompletionMessage => null;
+        public UpdatePackage LatestUpdate;
     }
 }
